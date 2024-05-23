89bdfc3b8a4f7fb01da5a02497bc549758dfb8ac
yourboyfred <fredrickkimathi26@gmail.com>
2024-05-23 10:46:30 +0300

    admin dashboard

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,8 +23,8 @@ defineProps({
     <Head title="Welcome" />
 
     <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100  dark:bg-gray-800 selection:bg-red-500 selection:text-white"
-    >
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-cover bg-gray-100  dark:bg-gray-800 selection:bg-red-500 selection:text-white"
+                style="background-image: url('https://www.cxtoday.com/wp-content/uploads/2021/05/Issue-Tracker.png');" >
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
             <Link
                 v-if="$page.props.auth.user"
@@ -36,14 +36,14 @@ defineProps({
             <template v-else>
                 <Link
                     :href="route('login')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    class="font-bold text-white hover:text-gray-900 dark:white dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-cyan-500"
                     >Log in</Link
                 >
 
                 <Link
                     v-if="canRegister"
                     :href="route('register')"
-                    class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    class="font-bold text-white pl-5 hover:text-gray-900 dark:white dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-cyan-500"
                     >Register</Link
                 >
             </template>
